f5a1955d2434cbfe125d7ef1808f3ca2150f33d7
MacBook Pro <macbookpro@macbooks-mbp.nvstu.edu>
2017-02-08 20:13:01 -0500

    commit 1

--- src/com/theironyard/novauc/Main.java
+++ src/com/theironyard/novauc/Main.java
@@ -1,8 +1,66 @@
 package com.theironyard.novauc;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+
+        int balance = 500;
+        System.out.println("What is your name?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        if (name.equals("") ) {
+            throw new Exception("invalid name response");
+        } else {
+            System.out.println("Welcome, " + name);
+        }
+
+
+        System.out.println("would you like to check a balance, make a withdrawl or cancel?");
+        String prompt = scanner.nextLine();
+        if (prompt.equalsIgnoreCase("check a balance")) {
+            System.out.println("your balance is " + balance);
+        }
+        else if (prompt.equalsIgnoreCase("cancel")) {
+            System.out.println("thank you and come again ");
+        }
+        else {
+            System.out.println("How much would like to withdraw?");
+            String withdrawl = scanner.nextLine();
+            int withdrawAmount = Integer.valueOf(withdrawl);
+            if (balance< withdrawAmount){
+                throw new Exception("balence exceeds amount available");
+            }
+            else {
+                balance = balance - withdrawAmount;
+                System.out.println("please take your cash");
+                System.out.println("your new balence is " + balance);
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
