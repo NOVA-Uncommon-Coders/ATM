c50470989e98394a623503f49669c4098d2da7e5
ALVIN WILLIAMS <ANVIL_OCTOPUS@alvins-mbp.nvstu.edu>
2017-03-01 17:43:58 -0500

    final ATM

--- src/com/theironyard/novauc/ATM.java
+++ src/com/theironyard/novauc/ATM.java
@@ -3,11 +3,17 @@ package com.theironyard.novauc;
 
 
 public class ATM {
-public static Customer cust1 = new Customer();
+public static Customer cust1 = new Customer("alvin", 1000.0);
+public static Balance balance = new Balance();
+
 
     public static void main(String[] args) throws Exception {
         cust1.prompt();
         cust1.menu();
+        double  balance;
+
     }
+
+
 
 }
